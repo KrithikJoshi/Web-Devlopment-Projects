fc58a482a5f894940cabf3dc63d1b3d8d6d9e466
KrithikJoshi <aimjkrithik@gmail.com>
2023-05-31 21:09:54 +0530

    button styling js placeholder updated

--- NumberGuessingGame/script.js
+++ NumberGuessingGame/script.js
@@ -1,3 +1,8 @@
 const myheading = document.querySelector("h1");
 myheading.textContent = "Number Guessing";
 
+const input = document.querySelector(".input");
+input.placeholder = "type the numbers here";
+
+const button = document.querySelector(".btn")
+
