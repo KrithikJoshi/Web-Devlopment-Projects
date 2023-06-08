582750ca81ea821a26d6dcee95d721bf6c4dd1c1
KrithikJoshi <aimjkrithik@gmail.com>
2023-06-08 13:52:24 +0530

    javascript_button_update

--- NumberGuessingGame/script.js
+++ NumberGuessingGame/script.js
@@ -4,5 +4,10 @@ myheading.textContent = "Number Guessing";
 const input = document.querySelector(".input");
 input.placeholder = "type the numbers here";
 
-const button = document.querySelector(".btn")
+const button = document.querySelector(".btn");
 
+function myfunction() {
+  var input = document.getElementById("input--id");
+  var button1 = document.getElementById("btn1");
+  input.innerHTML = input.innerHTML + button1.value;
+}
